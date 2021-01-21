2681689b7d66584e3701c2ebddc6d9cb9f608098
Daniel Araneda <daniel.araneda.h@gmail.com>
2021-01-21 16:45:23 -0300

    Commit inicial, solo navegacion de vistas

---
+++ js/config.js
@@ -0,0 +1,58 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+    $urlRouterProvider.otherwise("/index/main");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('index', {
+            abstract: true,
+            url: "/index",
+            templateUrl: "views/common/content.html",
+        })
+        .state('index.main', {
+            url: "/main",
+            templateUrl: "views/main.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('index.minor', {
+            url: "/minor",
+            templateUrl: "views/minor.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('index.dashboard', {
+            url: "/dashboard",
+            templateUrl: "views/dashboard.html",
+            data: { pageTitle: 'Tablero de reportes' }
+        })
+
+
+        .state('app', {
+            abstract: true,
+            url: "/app",
+            templateUrl: "views/common/content-app.html",
+        })
+        .state('app.home', {
+            url: "/home",
+            templateUrl: "views/app/home.html",
+            data: { pageTitle: 'Home' }
+        })
+
+}
+angular
+    .module('inspinia')
+    .config(config)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
